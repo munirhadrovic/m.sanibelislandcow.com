e8ecff5f7c81424e2690b87ae4a9845b39b38708
anes89cok <anes89cok@gmail.com>
2015-03-20 13:35:43 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="2163 Periwinkle Way"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Sanibel Island FL 33957";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica="FL"; // AKo ne znate skracenicu ostavite praznu
+	$telephone= 2394720606;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
